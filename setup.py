dfa55379dbdc99a50aef388dcddad0f8cac5e8d6
Justin Dierking <phnomcobra@gmail.com>
2021-02-18 16:31:03 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
-import sys
-
 from cx_Freeze import Executable
 from cx_Freeze import setup
 
-base = None
-if sys.platform == 'win32':
-    base = 'Win32GUI'
+build_exe_options = {
+    'packages': ['os', 'cherrypy', 'requests', 'sqlite3', 'Crypto']
+}
 
 setup( 
     name = 'stembot',
     version = '0.1',
-    description = 'stembot!',
-    options = {},
+    description = 'stembot',
+    options = {'build_exe': build_exe_options},
+    packages=['stembot'],
     executables = [
-        Executable('agcli', base=base),
-        Executable('agcsc', base=base),
-        Executable('agget', base=base),
-        Executable('aglookup', base=base),
-        Executable('agnet', base=base),
-        Executable('agping', base=base),
-        Executable('agput', base=base),
-        Executable('agtest', base=base),
-        Executable('config', base=base),
-        Executable('kvs', base=base),
-        Executable('server', base=base),
-        Executable('tvs', base=base),
+        Executable('agcli'),
+        Executable('agcsc'),
+        Executable('agget'),
+        Executable('aglookup'),
+        Executable('agnet'),
+        Executable('agping'),
+        Executable('agput'),
+        Executable('agtest'),
+        Executable('config'),
+        Executable('kvs'),
+        Executable('server'),
+        Executable('tvs')
     ]
 )
