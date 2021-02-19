11e454da0ca322974ce3cb13b2bca6602d6bf1cf
Justin Dierking <phnomcobra@gmail.com>
2021-02-18 22:38:31 -0500

    Trimmed unneeded options from setup.

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 from cx_Freeze import Executable
 from cx_Freeze import setup
 
-build_exe_options = {
-    'packages': ['os', 'cherrypy', 'requests', 'sqlite3', 'Crypto']
-}
-
 setup( 
     name = 'stembot',
     version = '0.1',
     description = 'stembot',
-    options = {'build_exe': build_exe_options},
     packages=['stembot'],
     executables = [
         Executable('agcli'),
