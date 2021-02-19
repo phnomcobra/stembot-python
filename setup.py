e34e086a77f1420d4f1c7876eec2aaf24f525681
Justin Dierking <phnomcobra@gmail.com>
2021-02-18 23:09:06 -0500

    Added msi options

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
+import sys
+
 from cx_Freeze import Executable
 from cx_Freeze import setup
+
+options = {
+    'bdist_msi': {
+        'upgrade_code': '{9AF7F942-6106-4B29-A2AE-551C738648E2}',
+        'add_to_path': True,
+        'initial_target_dir': r'[ProgramFilesFolder]\Stembot'
+    },
+    'build_exe': {
+        'include_msvcr': True
+    }
+}
 
 setup( 
     name = 'stembot',
     version = '0.1',
     description = 'stembot',
     packages=['stembot'],
+    options=options,
     executables = [
         Executable('agcli'),
         Executable('agcsc'),
